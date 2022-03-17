c4b6f3f79874cae4c68da1ade0139a1191a311e1
Prasham Patel <91484142+Prasham-Patel@users.noreply.github.com>
2022-03-17 11:57:11 -0400

    Add files via upload

---
+++ ROS_sim/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/purna/Quad_Simulation/src
+# Build directory: /home/purna/Quad_Simulation/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("fly_bot")
